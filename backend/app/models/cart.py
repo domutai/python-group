950bf3623d65513152c86a87df5229f0c8dfa5a5
aarongarciacr <aarongarcia4997@gmail.com>
2024-12-17 18:13:48 -0600

    Add relationship and foreign keys to model

--- backend/app/models/cart.py
+++ backend/app/models/cart.py
@@ -5,9 +5,14 @@ class Cart(db.Model):
     __tablename__ = 'carts'
 
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, nullable=False,)
-    productId = db.Column(db.Integer, nullable=False)
+    userId = db.Column(db.Integer,db.ForeignKey('users.id') nullable=False,)
+    productId = db.Column(db.Integer,db.ForeignKey('products.id') nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship('User', back_populates='carts')
+    product = db.relationship('Product', back_populates='carts')
+
+
 
     def to_dict(self):
         return {
