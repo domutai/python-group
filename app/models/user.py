76bf7b421538b10481f7c3b5de0e02bf4297194e
aarongarciacr <aarongarcia4997@gmail.com>
2024-12-20 09:52:16 -0600

    Prepare to deploy

--- app/models/user.py
+++ app/models/user.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
 
     carts = db.relationship('Cart', back_populates='user', cascade="all, delete-orphan")
     favorites = db.relationship('Favorite', back_populates='user', cascade="all, delete-orphan")
-
+    reviews = db.relationship('Review', back_populates='user', cascade="all, delete-orphan")
 
     @property
     def password(self):
