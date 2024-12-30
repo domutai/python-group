0a3248fadcb8d55897600b79691c8d5a20d7afda
aarongarciacr <aarongarcia4997@gmail.com>
2024-12-30 13:17:09 -0600

    npm run build

--- react-vite/src/components/Cart/CartPage.jsx
+++ react-vite/src/components/Cart/CartPage.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { thunkGetCart } from "../../redux/cart";
 
 function CartPage() {
