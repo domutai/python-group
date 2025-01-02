fd77823628a76fc9894ddd20407a1f4effa7df16
aarongarciacr <aarongarcia4997@gmail.com>
2025-01-02 15:01:40 -0600

    Cart Page css

--- react-vite/src/components/Cart/CartPage.jsx
+++ react-vite/src/components/Cart/CartPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { thunkGetCart } from "../../redux/cart";
+import "./CartPage.css";
 
 function CartPage() {
   // const navigate = useNavigate();
@@ -19,20 +20,53 @@ function CartPage() {
   }
 
   return (
-    <>
+    <div className="cart-page">
       <h1>Shopping Cart</h1>
       {cart?.length > 0 ? (
-        <ul>
+        <>
+          <p>
+            {cart?.length} {cart?.length > 1 ? "items" : "item"} in your cart
+          </p>
           {cart?.map((item) => (
-            <li key={item.id}>
-              {item.productId} {item.quantity}
-            </li>
+            <div className="cart-item" key={item.id}>
+              <div className="cart-item-left">
+                <h4 className="seller-name">Seller Name</h4>
+                <img
+                  src="https://via.placeholder.com/80" // Placeholder image for now
+                  alt={`Product ${item.productId}`}
+                  className="product-image"
+                />
+              </div>
+              <div className="cart-item-middle">
+                <h4 className="product-name">Product Name</h4>
+                <p className="product-tag">✔ Color: Black</p>
+                <p className="product-tag">✔ Size: Medium</p>
+                <div className="cart-item-actions">
+                  <input
+                    type="number"
+                    placeholder={`${item.quantity} ${
+                      item.quantity > 1 ? "Items" : "Item"
+                    }`}
+                  />
+                  <Link to={`/edit/${item.id}`} className="edit-link">
+                    Edit
+                  </Link>
+                  <Link to={`/remove/${item.id}`} className="remove-link">
+                    Remove
+                  </Link>
+                </div>
+              </div>
+              <div className="price">
+                <h3>$5.99</h3>
+                <p>($5.99 each)</p>
+              </div>
+            </div>
           ))}
-        </ul>
+        </>
       ) : (
         <p>Your cart is empty.</p>
       )}
-    </>
+    </div>
   );
 }
 
